ff5d17d58f3ca4e3d6b710c1aaace56c72ce1324
roneycm <roneychristian159@gmail.com>
2022-11-10 22:16:33 -0300

    creating users routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,13 +1,12 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import productsRouter from 'modules/products/routes/products.routes';
+import usersRouter from 'modules/users/routes/users.routes';
 
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/users', usersRouter);
 
-routes.get('/', (request: Request, response: Response) => {
-  return response.json({ message: 'Hello World' });
-  });
 
 export default routes;
